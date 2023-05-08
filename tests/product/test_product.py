2b6f5ae44c9e7268b66bb8aba8a4569485bde6c8
Thiago <programadorthiagolopes@gmail.com>
2023-05-08 18:53:36 -0300

    feat: Concluido Criacao de Teste 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import date
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_da_empresa = "Americanas"
+    nome_do_produto = "Caixa de Ovos"
+    data_de_fabricacao = date(2023, 2, 10)
+    data_de_validade = date(2023, 6, 10)
+    numero_de_serie = "123456"
+    instrucoes_de_armazenamento = "Guarda em local Fresco"
+    produto_um = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade, numero_de_serie, instrucoes_de_armazenamento)
+
+    assert produto_um.id == 1
+    assert produto_um.nome_do_produto == nome_do_produto
+    assert produto_um.nome_da_empresa == nome_da_empresa
+    assert (produto_um.data_de_fabricacao
+            == data_de_fabricacao.strftime("%Y-%m-%d"))
+    assert produto_um.data_de_validade == data_de_validade.strftime("%Y-%m-%d")
+    assert produto_um.numero_de_serie == numero_de_serie
+    assert (produto_um.instrucoes_de_armazenamento
+            == instrucoes_de_armazenamento)
