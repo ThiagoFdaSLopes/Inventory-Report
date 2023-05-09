c8c88ac69272e1dcba0a1301fa57e06fc0fe4585
Thiago <programadorthiagolopes@gmail.com>
2023-05-08 22:26:09 -0300

    feat: Product Report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,15 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto_um = Product(
+        1,
+        "Cesta",
+        "Casa",
+        "10-10-2023",
+        "10-12-2023", 122344, "na Mesa")
+
+    assert (
+        produto_um.__repr__() ==
+        "O produto Cesta fabricado em 10-10-2023 por Casa" +
+        " com validade até 10-12-2023 precisa ser armazenado na Mesa.")
